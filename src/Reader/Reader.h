6b281fccf5a340e5c9f15df6061e970325cb066a
Igor <igorsoroczynski@gmail.com>
2022-12-25 21:33:02 +0100

    Read raw data from /proc/stat and save it to struct array

--- src/Reader/Reader.h
+++ src/Reader/Reader.h
@@ -1,19 +1,24 @@
 #ifndef READER_H
 #define READER_H
 
+#include <stdint.h>
 
+#define MAX_NUMBER_OF_CORES 4
 
+typedef unsigned long long cputime_t;
 
-
-
-
-
-
-
-
-
-
-
+typedef struct cpuTimes_s
+{
+    cputime_t user;
+    cputime_t nice;
+    cputime_t system;
+    cputime_t idle;
+    cputime_t iowait;
+    cputime_t irq;
+    cputime_t softirq;
+    cputime_t steal;
+    cputime_t guest;
+} cpuTimes_s;
 
 
 
--- src/main.c
+++ src/main.c
@@ -8,29 +8,68 @@
 
 #include "Reader/Reader.h"
 
+#define DEBUG 1
+
+cpuTimes_s cpuTimesArr[MAX_NUMBER_OF_CORES] = {0};
+pthread_mutex_t mutexCpuTime;
 
 void* Reader(void* arg)
 {
-    printf("Hello from reader!\n");
-    int fd = -1;
+    FILE *file = NULL;
     
     for(;;)
     {
+        char* line_buff = NULL;
+        size_t line_len = 0;
+
         /*Get raw data from /proc/stat */
-        if((fd = open("/proc/stat", O_RDONLY)) < 0)
+        if((file = fopen("/proc/stat", "r")) == NULL)
         {
             printf("Error during file open!\n");
-            break;    
-        }
-
-
-
-
-        if(close(fd) != 0)
-        {
-            printf("Error during closing file!\n");
             break;
         }
+
+        pthread_mutex_lock(&mutexCpuTime);
+        while(getline(&line_buff, &line_len, file) != -1)
+        {
+            for(int core_idx = 0; core_idx < MAX_NUMBER_OF_CORES; core_idx++)
+            {
+                int ret = 0;
+                int cpu = 0;
+
+                ret = sscanf(line_buff, "cpu%d %llu %llu %llu %llu %llu %llu %llu %llu %llu",
+                &cpu,
+                &cpuTimesArr[core_idx].user,
+                &cpuTimesArr[core_idx].nice,
+                &cpuTimesArr[core_idx].system,
+                &cpuTimesArr[core_idx].idle,
+                &cpuTimesArr[core_idx].iowait,
+                &cpuTimesArr[core_idx].irq,
+                &cpuTimesArr[core_idx].softirq,
+                &cpuTimesArr[core_idx].steal,
+                &cpuTimesArr[core_idx].guest);
+
+                #ifdef DEBUG
+                if ((cpu == core_idx) && (ret == 10))
+                {
+                    printf("PROC: cpu=%u user=%llu nice=%llu system=%llu idle=%llu "
+                        "iowait=%llu irq=%llu softirq=%llu steal=%llu guest=%llu\n",
+                        cpu, cpuTimesArr[core_idx].user, cpuTimesArr[core_idx].nice, cpuTimesArr[core_idx].system, cpuTimesArr[core_idx].idle,
+                        cpuTimesArr[core_idx].iowait, cpuTimesArr[core_idx].irq, cpuTimesArr[core_idx].softirq, cpuTimesArr[core_idx].steal, cpuTimesArr[core_idx].guest);
+                }
+                #endif
+            }
+        }
+        pthread_mutex_unlock(&mutexCpuTime);
+
+        if(fclose(file) != 0)
+        {
+            printf("Error during fclose!\n");
+            break;
+        }
+        
+        free(line_buff);
+        sleep(1);
     }
 
     return arg;
@@ -40,6 +79,7 @@ void* Reader(void* arg)
 int main()
 {
     pthread_t th[5];
+    pthread_mutex_init(&mutexCpuTime, NULL);
 
     // Reader Task
     if(pthread_create(&th[0], NULL, Reader, NULL) != 0)
@@ -48,25 +88,13 @@ int main()
         goto CleanUp;
     }
 
-
-
-    // join
-    // for(int t = 0; t < 5; t++)
-    // {
-    //     if(pthread_join(th[t], NULL) != 0)
-    //     {
-    //         printf("Pthread_create error!\n");
-    //         return -1;
-    //     }
-    // }
-
     if(pthread_join(th[0], NULL) != 0)
     {
         printf("Pthread_create error!\n");
         return -1;
     }
 
-
+    pthread_mutex_destroy(&mutexCpuTime);
     printf("Cpu Usage Tracker \n");
     return 0;
 
